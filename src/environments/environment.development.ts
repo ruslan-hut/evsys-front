44847a60b7fcf66c570316bbdd9277815783f3e9
programador <rkhut@h2plt.energy>
2023-08-04 17:07:31 +0200

    changed api url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,14 +1,7 @@
 export const environment = {
-  apiUrl: 'https://hoot.com.ua:8800/api/v1',
+  apiUrl: 'https://wattbrews.me:9002/api/v1',
   readSysLog: '/syslog',
   readBackLog: '/backlog',
   chargePointList: '/chp',
-  wsUrl: 'wss://hoot.com.ua:8800/ws'
+  wsUrl: 'wss://wattbrews.me:9002/ws'
 };
-export const pusherConf = {
-  apiKey: 'a1f101fb40a32c47c791',
-  cluster: 'eu',
-  channel: 'sys_log',
-  event: 'call_event',
-  logToConsole: false
-}
