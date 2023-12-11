29ceb0f5016b90580f48136e78800922659a7f3e
programador <rkhut@h2plt.energy>
2023-12-11 11:20:14 +0100

    changed menu and edit visibility on user role

--- src/app/components/chargepoint/chargepoint.component.ts
+++ src/app/components/chargepoint/chargepoint.component.ts
@@ -1,22 +1,37 @@
-import {Component, Input} from "@angular/core"
+import {Component, Input, OnInit} from "@angular/core"
 import {Chargepoint} from "../../models/chargepoint";
 import { Router } from '@angular/router';
+import {AccountService} from "../../service/account.service";
 
 @Component({
   selector: 'app-chargepoint',
   templateUrl: './chargepoint.component.html',
   styleUrls: ['./chargepoint.component.css']
 })
-export class ChargepointComponent {
+export class ChargepointComponent implements OnInit {
   @Input() chargepoint: Chargepoint
 
+  isAdmin = false;
   details = false
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private accountService: AccountService,
+    ) { }
 
   editChargePoint() {
     const chargepointId = this.chargepoint.charge_point_id;
 
     this.router.navigate(['points-form', { id: chargepointId }]);
+  }
+
+  ngOnInit(): void {
+    this.accountService.user.subscribe(user => {
+      if (user) {
+        this.isAdmin = user.role === 'admin'
+      } else {
+        this.isAdmin = false;
+      }
+    });
   }
 }
