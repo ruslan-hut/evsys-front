deb2c7351609f95395c82a8e9f58f8fe8c8cdbe3
HootEgor <egor.hoot@gmail.com>
2024-01-17 12:27:52 +0100

    edit: chp-info view

--- src/app/components/chargepoint-info/chargepoint-info.component.ts
+++ src/app/components/chargepoint-info/chargepoint-info.component.ts
@@ -72,4 +72,27 @@ export class ChargepointInfoComponent implements OnInit{
     });
   }
 
+  getTimeDifference(date: string): string {
+    const eventTime = new Date(date);
+    const now = new Date();
+    const differenceInSeconds = Math.floor((now.getTime() - eventTime.getTime()) / 1000);
+
+    const days = Math.floor(differenceInSeconds / (3600 * 24));
+    const hours = Math.floor((differenceInSeconds % (3600 * 24)) / 3600);
+    const minutes = Math.floor((differenceInSeconds % 3600) / 60);
+
+    let result = '';
+    if (days > 0) {
+      result += `${days} d. `;
+    }
+    if (hours > 0) {
+      result += `${hours} h. `;
+    }
+    if (minutes > 0) {
+      result += `${minutes} min. `;
+    }
+
+    return result.length > 0 ? result : 'now';
+  }
+
 }
