c78df121075e7745faf41358ec04d8c59f8a605e
ruslanhut <dev@programmer.com.ua>
2024-11-08 22:34:08 +0100

    Improve token handling in account service
    Refactored the logic to distinguish between Firebase and local API authentication, ensuring tokens are properly handled. Added detailed comments for better code understanding and maintainability, and ensured logout occurs when no valid token is available.

--- src/app/service/account.service.ts
+++ src/app/service/account.service.ts
@@ -37,6 +37,7 @@ export class AccountService {
   // called on app initialization, after loading Firebase service
   afterFirebaseLoad() {
     const auth = getAuth();
+
     onAuthStateChanged(auth, (user) => {
       if (user) {
         this.updateToken();
@@ -47,26 +48,28 @@ export class AccountService {
     // listen for user data loading and update token
     this.user.subscribe(user => {
       if (user) {
-        if (user.token?.length == 32) {
-          if (environment.debug) {
-            console.log("Call API token update", user.username);
-          }
-          this.loginWithToken(user.token).subscribe(user => {
-              if (environment.debug) {
-                console.log("User authenticated", user.username);
-              }
-            }
-          )
-        }else{
+        if (auth.currentUser) { // firebase user
           if (environment.debug) {
             console.log("Call Firebase token update", user.username);
           }
           this.updateToken();
+        } else if (user.token) { // local user
+          if (environment.debug) {
+            console.log("Authenticate with stored token", user.username, ">>", user.token.substring(0, 5));
+          }
+          this.tokenSubject.next(user.token);
+          this.authState.next(true);
+        } else { // no token for local user
+          this.logout();
         }
       }
     })
   }
 
+  /**
+   * Updates the authentication token for the current user.
+   * Retrieves the ID token from Firebase and updates the tokenSubject and authState.
+   */
   private updateToken() {
     const auth = getAuth();
     auth.currentUser?.getIdToken().then((token) => {
@@ -90,26 +93,47 @@ export class AccountService {
     return this.userValue?.role === environment.operator;
   }
 
+  /**
+   * Re-authenticates the user.
+   * If the user is authenticated by Firebase, updates the authentication token.
+   * If the user is authenticated by local API, logs the user out.
+   */
   reAuthenticate() {
     if (environment.debug) {
       console.log("Reauthenticate called");
     }
-    this.updateToken();
+    const auth = getAuth();
+    if (auth.currentUser) {
+      this.updateToken();
+    } else {
+      this.logout();
+    }
   }
 
+  /**
+   * Local API authentication.
+   * Authenticates the user with the provided username and password.
+   * Sends a POST request to the authentication endpoint and updates the user state.
+   *
+   * @param username - The username of the user.
+   * @param password - The password of the user.
+   * @returns An Observable of the authenticated user.
+   */
   login(username: string, password: string) {
     return this.http.post<User>(`${environment.apiUrl}/users/authenticate`, {username, password})
       .pipe(map(user => {
         localStorage.setItem('user', JSON.stringify(user));
-        if (user.token && user.token != this.tokenSubject.value) {
-          this.tokenSubject.next(user.token);
-          this.userSubject.next(user);
-        }
-        this.authState.next(true)
+        this.userSubject.next(user);
         return user;
       }));
   }
 
+  /**
+   * Local API authentication with token.
+   *
+   * @param token - The token of the user.
+   * @returns An Observable of the authenticated user.
+   */
   loginWithToken(token: string) {
     return this.login("", token)
   }
@@ -127,6 +151,7 @@ export class AccountService {
     }
     // @ts-ignore
     this.userSubject.next(null);
+    this.tokenSubject.next(null);
     this.router.navigate(['/account/login']).then(() => {});
   }
 
