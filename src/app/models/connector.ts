672af91ce5af1964bb8f0fa8b0828ce416f31aed
ruslanhut <dev@programmer.com.ua>
2026-07-21 14:45:39 +0200

    add: current power limit in connector data

--- src/app/models/connector.ts
+++ src/app/models/connector.ts
@@ -11,6 +11,7 @@ export interface Connector {
   error_code: string,
   power: number,
   current_transaction_id: number,
+  current_power_limit: number,
 }
 
 export function getConnectorName(connector: Connector): string {
